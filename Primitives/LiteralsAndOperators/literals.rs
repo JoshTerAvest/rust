632e463695fb9f872b3e9577773a29ede2bf821f
Josh TerAvest <JoshTerAvest@Gmail.com>
2024-07-30 21:52:31 -0700

    Adding Literals and Operators

---
+++ Primitives/LiteralsAndOperators/literals.rs
@@ -0,0 +1,14 @@
+fn main() { 
+   println!("1 + 2 = {}", 1u32 + 2);
+
+   println!("1 - 2 = {}", 1i32 - 2);
+
+   println!("1e4 is {}, -2.5e-3 is {}", 1e4, -2.5e-3);
+
+
+   println!("true AND false is {}", true && false);
+   println!("true OR false is {}", true || false);
+   println!("NOT true is {}", !true);
+
+   println!("One million is written as {}", 1_000_000u32);
+}
